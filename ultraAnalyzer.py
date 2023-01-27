3d499f7a12b86cd2a2e1a45245e69d1f6194b9a0
Ryo Furukawa <furu.4se@gmail.com>
2023-01-27 22:07:40 +0900

    small fix

--- ultraAnalyzer.py
+++ ultraAnalyzer.py
@@ -4,13 +4,13 @@ import glob
 
 csv_files = glob.glob('*.csv')  # From AP-Acquisition
 
-sens = [27, 29, 30, 33, 36, 38, 40]  #300-900 Hz, in (nV/Pa)
+sens = [27, 29, 30, 33, 36, 38, 40]  # 300-900 Hz, in (nV/Pa)
 f_sens = np.arange(300, 900, 100)
 
 target = '.'
 
 for file in csv_files:
-    data = np.loadtxt(file, delimiter=',', skiprows=12, encoding='utf-8')
+    data = np.loadtxt(file, delimiter=',', skiprows=0, encoding='utf-8')
     
     idx = file.find(target)
     
@@ -18,6 +18,7 @@ for file in csv_files:
     t = data[:, 0]
     p = data[:, 1]
     
+    # Chose the suitable sensitivity
     f_round = round(f, -2)
     for i in sens:
         if f_round == f_sens[i]:
@@ -26,14 +27,13 @@ for file in csv_files:
     p /= 10  # pre amplifire
     p /= scale * 1e-9  #signal scale nV/Pa
 
-    #Figure
+    # Figure
     plt.plot(t * 1e6, p * 1e-3, color='black')
     plt.xlabel('Time (\u03bcs)')
-    plt.ylabel('Acoustic pressure [kPa]')
-    #plt.show()
+    plt.ylabel('Acoustic pressure (kPa)')
     plt.savefig(f'data/{f}.png')  #save
     plt.close()
 
-    #csv
+    # csv
     save_csv = np.c_[t, p]
     np.savetxt(f'data/{f}_pressure.csv', save_csv, delimiter=',')
